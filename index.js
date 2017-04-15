8e9acc528fc377f5a1e79b5b6450e39240477400
1412414 <thienphuvuong@gmail.com>
2017-04-15 23:03:03 +0700

    cap nhat moi nhat

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const pool = require('./lib/db');
 var session = require('express-session');
 var app = express();
 
-app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 }}));
+app.use(session({
+    secret: 'keyboard cat',
+}));
 app.use(express.static('public'));
 app.use('/components', express.static('bower_components'));
 app.use(bodyParser.urlencoded({
@@ -22,40 +24,80 @@ app.set('view engine', '.hbs');
 
 //routes
 app.get('/', function(req, res) {
-    
-    res.render('Home', {
-        layout: 'baitap1',
-        cssfile: 'Home',
-        activenavbar0: 'active',
-        activeleftmenu0: 'active'
-    });
+    if (req.session.username) {
+        res.render('Home', {
+            layout: 'baitap1',
+            cssfile: 'Home',
+            activenavbar0: 'active',
+            activeleftmenu0: 'active',
+            username: req.session.username
+        });
+    } else {
+        res.render('Home', {
+            layout: 'baitap1',
+            cssfile: 'Home',
+            activenavbar0: 'active',
+            activeleftmenu0: 'active'
+        });
+    }
+
 });
 
 app.get('/Album', function(req, res) {
-    res.render('Album', {
-        layout: 'baitap1',
-        cssfile: 'Album',
-        activenavbar1: 'active',
-        activeleftmenu1: 'active'
-    });
+    if (req.session.username) {
+        res.render('Album', {
+            layout: 'baitap1',
+            cssfile: 'Album',
+            activenavbar1: 'active',
+            activeleftmenu1: 'active',
+            username: req.session.username
+        });
+    } else {
+        res.render('Album', {
+            layout: 'baitap1',
+            cssfile: 'Album',
+            activenavbar1: 'active',
+            activeleftmenu1: 'active'
+        });
+    }
 });
 
 app.get('/About', function(req, res) {
-    res.render('About', {
-        layout: 'baitap1',
-        cssfile: 'About',
-        activenavbar2: 'active',
-        activeleftmenu2: 'active'
-    });
+    if (req.session.username) {
+        res.render('About', {
+            layout: 'baitap1',
+            cssfile: 'About',
+            activenavbar2: 'active',
+            activeleftmenu2: 'active',
+            username: req.session.username
+        });
+    } else {
+        res.render('About', {
+            layout: 'baitap1',
+            cssfile: 'About',
+            activenavbar2: 'active',
+            activeleftmenu2: 'active'
+        });
+    }
 });
 
 app.get('/Blog', function(req, res) {
-    res.render('Blog', {
-        layout: 'baitap1',
-        cssfile: 'Blog',
-        activenavbar3: 'active',
-        activeleftmenu3: 'active'
-    });
+    if (req.session.username) {
+        res.render('Blog', {
+            layout: 'baitap1',
+            cssfile: 'Blog',
+            activenavbar3: 'active',
+            activeleftmenu3: 'active',
+            username: req.session.username
+        });
+    } else {
+        res.render('Blog', {
+            layout: 'baitap1',
+            cssfile: 'Blog',
+            activenavbar3: 'active',
+            activeleftmenu3: 'active'
+        });
+    }
 });
 
 app.get('/DangKy', function(req, res) {
@@ -71,41 +113,60 @@ app.get('/DangKyThatBai', function(req, res) {
 });
 
 app.post('/DangKy', function(req, res) {
-    pool.query("select idnguoidung from nguoidung where tendangnhap = $1", [req.body.user.tenDangNhap], function(err, result) {
+    if (req.body.user.tenDangNhap.length < 10) {
+        res.render('DangKy', {
+            errorMessage: 'Tên đăng nhập không được ít hơn 10 ký tự'
+        });
+    } else {
+        if (req.body.user.matKhau.length < 10) {
+            res.render('DangKy', {
+                errorMessage: 'Mật khẩu không được ít hơn 10 ký tự'
+            });
+        } else {
+            pool.query("select idnguoidung from nguoidung where tendangnhap = $1", [req.body.user.tenDangNhap], function(err, result) {
+                if (err) {
+                    return console.error('error running query', err);
+                }
+                if (result.rowCount == 1) {
+
+                    req.session.exists = true;
+                    res.redirect('/DangKyThatBai');
+                } else {
+                    pool.query("INSERT INTO nguoidung (tennguoidung, gioitinh, ngaysinh, matkhau, tendangnhap, email) VALUES ($1,$2, $3, $4, $5, $6)", [req.body.user.hoTen, req.body.user.gioiTinh, req.body.user.ngaySinh, req.body.user.matKhau, req.body.user.tenDangNhap, req.body.user.email], function(err, result2) {
+                        res.render('DangKy', {
+                            detailMessage: 'Đăng ký thành công'
+                        });
+                    });
+                }
+            });
+        }
+    }
+});
+
+app.post('/DangNhap', function(req, res) {
+    pool.query("select tennguoidung from nguoidung where tendangnhap = $1 and matkhau = $2", [req.body.user.tendangnhap, req.body.user.matkhau], function(err, result) {
         if (err) {
             return console.error('error running query', err);
         }
-        console.log(result);
-        if (result.rowCount == 1) {
 
-            req.session.exists = true;
-            res.redirect('/DangKyThatBai');
+        if (result.rowCount == 1) {
+            req.session.username = req.body.user.tendangnhap;
+            res.redirect('/');
         } else {
-            pool.query("INSERT INTO nguoidung (tennguoidung, gioitinh, ngaysinh, matkhau, tendangnhap, email) VALUES ($1,$2, $3, $4, $5, $6)", [req.body.user.hoTen, req.body.user.gioiTinh, req.body.user.ngaySinh, req.body.user.matKhau, req.body.user.tenDangNhap, req.body.user.email], function(err, result2) {
-                res.render('DangKy', {
-                    detailMessage: 'Đăng ký thành công'
-                });
-            });
+
         }
     });
 });
 
-app.post('/DangNhap', function(req, res) {
-  pool.query("select tennguoidung from nguoidung where tendangnhap = $1 and matkhau = $2", [req.body.user.tendangnhap, req.body.user.matkhau], function(err, result) {
-      if (err) {
-          return console.error('error running query', err);
-      }
-
-      if (result.rowCount == 1)
-      {
-        req.session.username = req.body.user.tendangnhap;
-        res.redirect('/');
-      }
-      else {
-
-      }
-  });
-});
+app.get('/DangXuat', function(req, res) {
+    req.session.destroy(function(err) {
+        if (err) {
+            console.error(err);
+        } else {
+            res.redirect('/');
+        }
+    })
+})
 
 var port = 3000;
 //start server
